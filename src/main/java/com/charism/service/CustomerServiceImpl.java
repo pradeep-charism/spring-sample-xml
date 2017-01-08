5ad57cd39c8130e9d60d0ce3672d6d1fee2c8463
pradeep-charism <pradeep.charism@gmail.com>
2017-01-08 15:48:51 +0800

    constructor injection

--- src/main/java/com/charism/service/CustomerServiceImpl.java
+++ src/main/java/com/charism/service/CustomerServiceImpl.java
@@ -13,16 +13,16 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
 
-    public CustomerRepository getCustomerRepository() {
-        return customerRepository;
-    }
-
-    public void setCustomerRepository(CustomerRepository customerRepository) {
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     @Override
     public List<Customer> findAll(){
         return customerRepository.findAll();
+    }
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 }
